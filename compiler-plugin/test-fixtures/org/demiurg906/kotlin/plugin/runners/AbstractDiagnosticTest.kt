11c3328cd1d815941f8a7b5a60e805eecccfd1e2
Brian Norman <brian.norman@jetbrains.com>
2025-05-19 08:44:15 -0500

    Use standard base tests for diagnostic and box tests

--- compiler-plugin/test-fixtures/org/demiurg906/kotlin/plugin/runners/AbstractDiagnosticTest.kt
+++ compiler-plugin/test-fixtures/org/demiurg906/kotlin/plugin/runners/AbstractDiagnosticTest.kt
@@ -1,20 +1,45 @@
 package org.demiurg906.kotlin.plugin.runners
 
+import org.demiurg906.kotlin.plugin.services.ExtensionRegistrarConfigurator
+import org.demiurg906.kotlin.plugin.services.PluginAnnotationsProvider
 import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
-import org.jetbrains.kotlin.test.directives.configureFirParser
+import org.jetbrains.kotlin.test.directives.CodegenTestDirectives
+import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
+import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
+import org.jetbrains.kotlin.test.runners.AbstractFirPhasedDiagnosticTest
 import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
 import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
 
-abstract class AbstractDiagnosticTest : BaseTestRunner() {
-    override fun configure(builder: TestConfigurationBuilder) {
-        with(builder) {
-            commonFirWithPluginFrontendConfiguration()
-            configureFirParser(FirParser.Psi)
-        }
-    }
-
+open class AbstractDiagnosticTest : AbstractFirPhasedDiagnosticTest(FirParser.LightTree) {
     override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
         return EnvironmentBasedStandardLibrariesPathProvider
+    }
+
+    override fun configure(builder: TestConfigurationBuilder) {
+        super.configure(builder)
+
+        with(builder) {
+            /*
+             * Containers of different directives, which can be used in tests:
+             * - ModuleStructureDirectives
+             * - LanguageSettingsDirectives
+             * - DiagnosticsDirectives
+             * - FirDiagnosticsDirectives
+             *
+             * All of them are located in `org.jetbrains.kotlin.test.directives` package
+             */
+            defaultDirectives {
+                +FirDiagnosticsDirectives.FIR_DUMP
+                +JvmEnvironmentConfigurationDirectives.FULL_JDK
+
+                +CodegenTestDirectives.IGNORE_DEXING // Avoids loading R8 from the classpath.
+            }
+
+            useConfigurators(
+                ::PluginAnnotationsProvider,
+                ::ExtensionRegistrarConfigurator
+            )
+        }
     }
 }
--- compiler-plugin/test-gen/org/demiurg906/kotlin/plugin/runners/DiagnosticTestGenerated.java
+++ compiler-plugin/test-gen/org/demiurg906/kotlin/plugin/runners/DiagnosticTestGenerated.java
@@ -4,6 +4,7 @@ package org.demiurg906.kotlin.plugin.runners;
 
 import com.intellij.testFramework.TestDataPath;
 import org.jetbrains.kotlin.test.util.KtTestUtil;
+import org.jetbrains.kotlin.test.TargetBackend;
 import org.jetbrains.kotlin.test.TestMetadata;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +18,7 @@ import java.util.regex.Pattern;
 public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
   @Test
   public void testAllFilesPresentInDiagnostics() {
-    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics"), Pattern.compile("^(.+)\\.kt$"), null, true);
+    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
   }
 
   @Test
